ca6a689dcc12befddb19269e53237fb54dfa23a8
MyUtd4Life <mail@jordidekker.com>
2014-07-27 03:20:22 +0200

    Responsive navigatie

--- index.php
+++ index.php
@@ -9,28 +9,45 @@ require('php/config/conf.Default.php');
 	<script src="js/documentready.js"></script>
 </head>
 <body class="grid">
-	<div class="actions">
-		<button type="button" data-type="grid"><span class="entypo-layout"></span> Grid View</button>
-		<button type="button" data-type="list"><span class="entypo-list"></span> List View</button>
-	</div>
-
-	<div class="story-wrapper">
-		<?php
-        // Load the eventmanager and request all events
-        $eventManager = EventManager::getInstance();
-        EventFactory::getInstance();
-		foreach($eventManager->getEvents() as $event) {
-			?>
-			<div class="story">
-				<div class="story-image"><img src="http://placehold.it/500x500"></div>
-				<div class="story-title"><?php echo $event->getName(); ?></div>
-                <div class="story-date"><?php echo $event->getStartDate(); ?></div>
-				<div class="story-desc"><?php echo $event->getDescription(); ?></div>
+	<nav role="navigation">
+		<span class="entypo-menu" id="toggle-menu"></span>  
+		<div class="logo">Planner</div>
+		
+		<ul>
+			<li><a href="#">About</a></li>
+			<li><a href="#">Features</a></li>
+			<li><a href="#">Explore</a></li>
+			<li><a href="#">Support</a></li>
+			<li><a href="#">Sign Up</a></li>
+			<div class="actions">
+				<button type="button" data-type="grid"><span class="entypo-layout"></span> Grid View</button>
+				<button type="button" data-type="list"><span class="entypo-list"></span> List View</button>
 			</div>
-			<?php
-		} 
-		?>
+		</ul>
 
+	</nav>  
+
+	<div class="container">
+
+
+		<div class="story-wrapper">
+			<?php
+        // Load the eventmanager and request all events
+			$eventManager = EventManager::getInstance();
+			EventFactory::getInstance();
+			foreach($eventManager->getEvents() as $event) {
+				?>
+				<div class="story">
+					<div class="story-image"><img src="http://placehold.it/500x500"></div>
+					<div class="story-title"><?php echo $event->getName(); ?></div>
+					<div class="story-date"><?php echo $event->getStartDate(); ?></div>
+					<div class="story-desc"><?php echo $event->getDescription(); ?></div>
+				</div>
+				<?php
+			} 
+			?>
+
+		</div>
 	</div>
 </body>
 </html>
--- js/documentready.js
+++ js/documentready.js
@@ -1,22 +1,62 @@
 $(document).ready(function () {
-    helloWorld();
-    listOrGrid();
+  helloWorld();
+  listOrGrid();
+
+  menuSetup();
+
+
 });
 
 function helloWorld() {
-    console.log('Hello world');
+  console.log('Hello world');
 }
 
 function listOrGrid() {
-      $('button').on('click',function(){
-  
-    var type = $(this).data('type');
+  $('button').on('click',function(){
     
-    if ( type === "list" ) {
+    var type = $(this).data('type');
+    var width = $( document ).width();
+    
+    if ( type === "list") {
       $('body').removeClass('grid');    
     } else if ( type === "grid" ) {
       $('body').addClass('grid');
-    }
-  
+    } 
   });
+
+  $(window).resize(function() {
+    if ($(window).width() <= 800) {
+     $('body').removeClass('grid');
+   }
+   else {
+    $('body').addClass('grid');
+  }
+});
+}
+
+function menuSetup(){
+  var w = $(window).width(),
+  toggle    = $('#toggle-menu'),
+  menu    = $('nav ul'),
+  hasChild = $('.has-child'),
+  dropdown = $('.dropdown');
+
+  $(function() {
+    $(toggle).on('click', function(e) {
+      e.preventDefault();
+      menu.toggle();
+    });
+    
+    $(hasChild).click(function(e) {
+      e.preventDefault();
+      dropdown.toggle();
+    });
+  });
+
+  $(window).resize(function(){
+    if(w > 320 && menu.is(':hidden')) {
+      menu.removeAttr('style');}
+    });
+
+
 }
