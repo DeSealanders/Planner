b443046364920ab9005a2d806be9d0e6d9b60a2a
Baabah <mail@peterton.nl>
2014-08-03 20:13:43 +0200

    Init volgorde gecorrigeerd

--- php/functions/func.init.php
+++ php/functions/func.init.php
@@ -3,10 +3,12 @@
  * This function is called whenever a page is loaded
  */
 
-// Call in order to load all events from the database
-EventFactory::getInstance()->loadEvents();
 
 // If a user used their pagelink, log this user in
 if(isset($_GET['pagelink'])) {
     UserManager::getInstance()->setUser($_GET['pagelink']);
 }
+
+// Call in order to load all events from the database
+// Important: User should be loaded before loading events
+EventFactory::getInstance()->loadEvents();
